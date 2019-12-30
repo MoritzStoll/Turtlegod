92ea4577aa85e2a9c1e452c1d74c9c89ae66e14c
MoritzStoll <moe-stoll@web.de>
2019-12-30 15:12:52 +0100

    finished

--- src/eventfunctions/setSpaceShipState.js
+++ src/eventfunctions/setSpaceShipState.js
@@ -10,12 +10,9 @@ function setSpaceShipState() {
 
     }
 
-    var volume = 0.2;
     if (arcadeState.music) {
-        if (arcadeState.volumeHigh) {
-            volume = 1.0;
-        } 
-        soundscape.getSounds()["src/sound/files/Music.mp3"].setVolume(2.0);
+
+        soundscape.getSounds()["src/sound/files/Music.mp3"].setVolume(1.0);
         soundscape.getSounds()["src/sound/files/Music.mp3"].play();
 
 
